f1c321b3b9e15703d4cfd5f5d75d75eb4c71d6c2
Francisco-Fortes <fortesfco@gmail.com>
2023-03-12 07:43:59 +0100

    Recipes CRUD
    WIP: Cloudinary

--- src/api/recipes/index.js
+++ src/api/recipes/index.js
@@ -1,20 +1,25 @@
 import express from "express";
 import createHttpError from "http-errors";
-import RecipessModel from "./model.js";
+import RecipesModel from "./model.js";
 
 const recipesRouter = express.Router();
 
 recipesRouter.post("/", async (req, res, next) => {
   try {
-    const recipes = await RecipesModel.find();
-    res.send(recipes);
+    const newRecipe = await RecipesModel(req.body);
+    newRecipe.save();
+    console.log(newRecipe._id);
+    res.send(newRecipe._id);
   } catch (error) {
+    console.log(error);
     next(error);
   }
 });
-
+//GET ALL RECIPES
 recipesRouter.get("/", async (req, res, next) => {
   try {
+    const allRecipes = await RecipesModel.find();
+    res.send(allRecipes);
   } catch (error) {
     next(error);
   }
@@ -22,6 +27,14 @@ recipesRouter.get("/", async (req, res, next) => {
 
 recipesRouter.get("/:recipeId", async (req, res, next) => {
   try {
+    const recipe = await RecipesModel.findById(req.params.recipeId);
+    if (recipe) {
+      res.send(recipe);
+    } else {
+      next(
+        createHttpError(404, `Recipe with id ${req.params.userId} not found!`)
+      );
+    }
   } catch (error) {
     next(error);
   }
@@ -29,6 +42,18 @@ recipesRouter.get("/:recipeId", async (req, res, next) => {
 
 recipesRouter.put("/:recipeId", async (req, res, next) => {
   try {
+    const recipeToModify = await RecipesModel.findByIdAndUpdate(
+      req.params.recipeId,
+      req.body,
+      { new: true }
+    );
+    if (recipeToModify) {
+      res.send(recipeToModify);
+    } else {
+      next(
+        createHttpError(404, `Recipe with id ${req.params.userId} not found!`)
+      );
+    }
   } catch (error) {
     next(error);
   }
@@ -36,6 +61,16 @@ recipesRouter.put("/:recipeId", async (req, res, next) => {
 
 recipesRouter.delete("/:recipeId", async (req, res, next) => {
   try {
+    const recipeToDelete = await RecipesModel.findByIdAndDelete(
+      req.params.recipeId
+    );
+    if (recipeToDelete) {
+      res.send("Recipe succesfully deleted");
+    } else {
+      next(
+        createHttpError(404, `Recipe with id ${req.params.userId} not found!`)
+      );
+    }
   } catch (error) {
     next(error);
   }
