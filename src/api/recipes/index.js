6e75663a016c27234fc944589c06aa40d84c078f
Francisco-Fortes <fortesfco@gmail.com>
2023-03-20 23:55:18 +0100

    recipesRouter updated

--- src/api/recipes/index.js
+++ src/api/recipes/index.js
@@ -1,36 +1,27 @@
 import express from "express";
 import createHttpError from "http-errors";
+import { JWTAuthMiddleware } from "../users/utils/jwtAuth.js";
 import RecipesModel from "./model.js";
+import cloudinaryUploader from "./utils/images/imageUploader.js";
 
 const recipesRouter = express.Router();
 
 //Data from the FE
-const saveRecipeToDatabase = async (recipeData) => {
-  try {
-    const newRecipe = await RecipesModel(recipeData);
-    console.log(recipeData);
-    return newRecipe.save();
-  } catch (error) {
-    console.log(error);
-    throw createHttpError(500, "Could not save recipe");
-  }
-};
-
-recipesRouter.post("/", async (req, res, next) => {
-  try {
-    const newRecipe = await saveRecipeToDatabase(req.body);
-    console.log(newRecipe._id);
-    res.send(newRecipe._id);
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
-});
+// const saveRecipeToDatabase = async (recipeData) => {
+//   try {
+//     const newRecipe = await RecipesModel(recipeData);
+//     console.log(recipeData);
+//     await newRecipe.save();
+//     return newRecipe._id;
+//   } catch (error) {
+//     console.log(error);
+//     throw createHttpError(500, "Could not save recipe");
+//   }
+// };
 
 // recipesRouter.post("/", async (req, res, next) => {
 //   try {
-//     const newRecipe = await RecipesModel(req.body);
-//     newRecipe.save();
+//     const newRecipe = await saveRecipeToDatabase(req.body);
 //     console.log(newRecipe._id);
 //     res.send(newRecipe._id);
 //   } catch (error) {
@@ -38,6 +29,18 @@ recipesRouter.post("/", async (req, res, next) => {
 //     next(error);
 //   }
 // });
+
+recipesRouter.post("/", async (req, res, next) => {
+  try {
+    const newRecipe = await RecipesModel(req.body);
+    newRecipe.save();
+    console.log(newRecipe._id);
+    res.send(newRecipe._id);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
 
 //GET ALL RECIPES
 recipesRouter.get("/", async (req, res, next) => {
@@ -75,7 +78,7 @@ recipesRouter.put("/:recipeId", async (req, res, next) => {
       res.send(recipeToModify);
     } else {
       next(
-        createHttpError(404, `Recipe with id ${req.params.userId} not found!`)
+        createHttpError(404, `Recipe with ID ${req.params.recipeId} not found`)
       );
     }
   } catch (error) {
@@ -99,5 +102,30 @@ recipesRouter.delete("/:recipeId", async (req, res, next) => {
     next(error);
   }
 });
+
+recipesRouter.post(
+  "/:recipeId/picture",
+  JWTAuthMiddleware,
+  cloudinaryUploader.single("picture"),
+  async (req, res, next) => {
+    try {
+      if (req.user) {
+        const recipe = await RecipesModel.findByIdAndUpdate(
+          req.recipe._id,
+          { picture: req.file.path },
+          { new: true }
+        );
+        res.status(201).send(recipe);
+      } else {
+        next(createError(404, `You need to log in order to post a recipe`));
+      }
+      if (!recipe)
+        next(createError(404, `Recipe with ID ${req.recipe._id} not found`));
+    } catch (error) {
+      res.send(error);
+      next(error);
+    }
+  }
+);
 
 export default recipesRouter;
