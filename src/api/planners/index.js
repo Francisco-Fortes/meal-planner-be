29cae289ca70a1ef1fad84f10353e0b1c83fd9c3
Francisco-Fortes <fortesfco@gmail.com>
2023-04-04 23:58:30 +0200

    Planners index,js updated

--- src/api/planners/index.js
+++ src/api/planners/index.js
@@ -7,7 +7,9 @@ const plannersRouter = express.Router();
 
 plannersRouter.get("/", JWTAuthMiddleware, async (req, res, next) => {
   try {
-    const planners = await PlannersModel.find({ author: req.user._id });
+    const planners = await PlannersModel.find({
+      author: req.user._id,
+    });
     if (!planners) {
       return res.status(404).json({ message: "User has not created any plan" });
     }
@@ -18,9 +20,59 @@ plannersRouter.get("/", JWTAuthMiddleware, async (req, res, next) => {
   }
 });
 
+// plannersRouter.get("/", JWTAuthMiddleware, async (req, res, next) => {
+//   try {
+//     const planners = await PlannersModel.find({ author: req.user._id });
+//     if (!planners) {
+//       return res.status(404).json({ message: "User has not created any plan" });
+//     }
+//     res.status(200).json(planners);
+//   } catch (error) {
+//     console.log(error);
+//     next(error);
+//   }
+// });
+
+//WORKING ON POSTMAN
+// plannersRouter.get("/", JWTAuthMiddleware, async (req, res, next) => {
+//   try {
+//     const planners = await PlannersModel.find();
+//     if (!planners) {
+//       return res.status(404).json({ message: "User has not created any plan" });
+//     }
+//     if (planners[0].author.toString() !== req.user._id.toString()) {
+//       return res
+//         .status(403)
+//         .json({ message: "You are not authorized to access this planner" });
+//     }
+
+//     res.status(200).json(planners);
+//   } catch (error) {
+//     console.log(error);
+//     next(error);
+//   }
+// });
+
 plannersRouter.get("/:plannerId", JWTAuthMiddleware, async (req, res, next) => {
   try {
-    const planner = await PlannersModel.findById(req.params.plannerId);
+    const planner = await PlannersModel.findById(req.params.plannerId).populate(
+      {
+        path: "Monday Tuesday Wednesday Thursday Friday Saturday Sunday",
+        select: "",
+      }
+    );
+    //Same as:
+    //   [
+    //     { path: "Monday", select: "" },
+    //     { path: "Tuesday", select: "" },
+    //     { path: "Wednesday", select: "" },
+    //     { path: "Thursday", select: "" },
+    //     { path: "Friday", select: "" },
+    //     { path: "Saturday", select: "" },
+    //     { path: "Sunday", select: "" },
+    //   ]
+    // );
+
     if (!planner) {
       return res.status(404).json({ message: "Planner not found" });
     }
@@ -31,7 +83,7 @@ plannersRouter.get("/:plannerId", JWTAuthMiddleware, async (req, res, next) => {
         .json({ message: "You are not authorized to access this planner" });
     }
 
-    res.status(200).json(planner);
+    res.status(200).send(planner);
   } catch (error) {
     console.log(error);
     next(error);
